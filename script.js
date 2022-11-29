5f0299e01d7d8838371138ccdcd52b7abda9bcb4
Timofey Krivenko <tikrivenko@gmail.com>
2022-11-29 22:57:59 +0300

    html and css fix

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 (() => {
     const questionsItems = document.querySelectorAll(".questions__item");
+    const schemeCards = document.querySelectorAll(".scheme__card");
+    const schemeClasses = ["schemeone", "schemetwo", "schemethree", "schemesix", "schemefive", "schemefour"];
+    const backcallButtons = document.querySelectorAll(".backcall__btn");
+    const popup = document.querySelector(".modular");
 
     questionsItems.forEach(item => item.addEventListener("click", e => {
         e.preventDefault();
@@ -8,11 +12,38 @@
         target.classList.toggle("open");
     }))
 
+    function resizeSchemes() {
+        let mediaMatchLess = window.matchMedia("(max-width: 1400px)");
+        let mediaMatchMore = window.matchMedia("(min-width: 1400px)");
+        if (mediaMatchLess.matches) {
+            schemeCards.forEach(item => {
+                item.classList.remove(...schemeClasses)
+            })
+        };
+        if (mediaMatchMore.matches) {
+            for (let i = 0; i < schemeCards.length; i++) {
+                schemeCards[i].classList.add(schemeClasses[i])
+            }
+        }
+    }
+
+    function closePopup(e) {
+        let field = e.target;
+        if (field.className === "modular") popup.style.visibility = "hidden";
+    };
+
+    window.addEventListener("resize", resizeSchemes);
+    window.addEventListener("DOMContentLoaded", resizeSchemes);
+    backcallButtons.forEach(item => {
+        item.addEventListener("click", () => {
+            popup.style.visibility = "visible";
+        })
+    })
+    popup.addEventListener("click", closePopup)
 
     const swiper = new Swiper('.swiper', {
         // Optional parameters
         effect: 'slide',
-        centeredSlides: true,
         loop: true,
 
         // Navigation arrows
@@ -22,11 +53,8 @@
         },
 
         breakpoints: {
-            320: {
+            550: {
                 slidesPerView: 1,
-            },
-            480: {
-                slidesPerView: 2,
                 spaceBetween: 20
             },
             640: {
@@ -34,7 +62,7 @@
                 spaceBetween: 30
             },
             1400: {
-                slidesPerView: 4,
+                slidesPerView: 3,
                 spaceBetween: 18
             },
             1550: {
